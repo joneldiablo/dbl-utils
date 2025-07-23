e8a9738b23bc650fff0430100d245156843b28c4
jonathan diego rdz <jon_eldiablo@hotmail.com>
2025-07-23 12:28:21 -0600

    unflatten error test

--- __tests__/flat.test.ts
+++ __tests__/flat.test.ts
@@ -1,92 +1,100 @@
 import { flatten, unflatten } from "../src/flat";
 
-
-describe('flatten', () => {
-  it('should flatten a simple object', () => {
+describe("flatten", () => {
+  it("should flatten a simple object", () => {
     const input = { a: 1, b: { c: 2, d: 3 } };
-    const expected = { 'a': 1, 'b.c': 2, 'b.d': 3 };
+    const expected = { a: 1, "b.c": 2, "b.d": 3 };
     const result = flatten(input);
     expect(result).toEqual(expected);
   });
 
-  it('should flatten an object with arrays', () => {
+  it("should flatten an object with arrays", () => {
     const input = { a: [1, 2, { b: 3 }] };
-    const expected = { 'a.0': 1, 'a.1': 2, 'a.2.b': 3 };
+    const expected = { "a.0": 1, "a.1": 2, "a.2.b": 3 };
     const result = flatten(input);
     expect(result).toEqual(expected);
   });
 
-  it('should omit specified keys', () => {
+  it("should omit specified keys", () => {
     const input = { a: [1, 4, 5], b: { c: 2, d: 3 } };
-    const expected = { 'a.0': 1, 'a.1': 4, 'a.2': 5, b: { c: 2, d: 3 } };
-    const result = flatten(input, { ommit: ['b'] });
+    const expected = { "a.0": 1, "a.1": 4, "a.2": 5, b: { c: 2, d: 3 } };
+    const result = flatten(input, { ommit: ["b"] });
     expect(result).toEqual(expected);
   });
 
-  it('should use custom delimiter', () => {
+  it("should use custom delimiter", () => {
     const input = { a: 1, b: { c: 2, d: 3 } };
-    const expected = { 'a': 1, 'b-c': 2, 'b-d': 3 };
-    const result = flatten(input, { delimiter: '-' });
+    const expected = { a: 1, "b-c": 2, "b-d": 3 };
+    const result = flatten(input, { delimiter: "-" });
     expect(result).toEqual(expected);
   });
 
-  it('should omit arrays if specified', () => {
+  it("should omit arrays if specified", () => {
     const input = { a: [1, 2, { b: 3 }] };
-    const expected = { 'a': [1, 2, { b: 3 }] };
+    const expected = { a: [1, 2, { b: 3 }] };
     const result = flatten(input, { ommitArrays: true });
     expect(result).toEqual(expected);
   });
 
-  it('should apply custom omit function', () => {
+  it("should apply custom omit function", () => {
     const input = { a: 1, b: { c: { e: 2 }, d: 3 } };
-    const expected = { 'a': 1, 'b.c': { e: 2 }, 'b.d': 3 };
-    const result = flatten(input, { ommitFn: (key) => key === 'c' });
+    const expected = { a: 1, "b.c": { e: 2 }, "b.d": 3 };
+    const result = flatten(input, { ommitFn: (key) => key === "c" });
     expect(result).toEqual(expected);
   });
 });
 
-describe('unflatten', () => {
-  it('should unflatten a simple object', () => {
-    const input = { 'a': 1, 'b.c': 2, 'b.d': 3 };
+describe("unflatten", () => {
+  it("should unflatten a simple object", () => {
+    const input = { a: 1, "b.c": 2, "b.d": 3 };
     const expected = { a: 1, b: { c: 2, d: 3 } };
     const result = unflatten(input);
     expect(result).toEqual(expected);
   });
 
-  it('should unflatten an object with arrays', () => {
-    const input = { 'a.0': 1, 'a.1.0': 2, 'a.2.b': [3] };
+  it("should unflatten an object with arrays", () => {
+    const input = { "a.0": 1, "a.1.0": 2, "a.2.b": [3] };
     const expected = { a: [1, [2], { b: [3] }] };
     const result = unflatten(input);
     console.log(result);
     expect(result).toEqual(expected);
   });
 
-  it('should unflatten an object with arrays at the end', () => {
+  it("should unflatten an object with arrays at the end", () => {
     const input = {
-      'note/to/0': 'John',
-      'note/from/0': 'Jane',
-      'note/heading/0/$': { attr: 'value' },
-      'note/uuid/0': '1234-5678-91011'
+      "note/to/0": "John",
+      "note/from/0": "Jane",
+      "note/heading/0/$": { attr: "value" },
+      "note/uuid/0": "1234-5678-91011",
     };
     const expected = {
       note: {
-        to: ['John'],
-        from: ['Jane'],
-        heading: [{ $: { attr: 'value' } }],
-        uuid: ['1234-5678-91011']
-      }
+        to: ["John"],
+        from: ["Jane"],
+        heading: [{ $: { attr: "value" } }],
+        uuid: ["1234-5678-91011"],
+      },
     };
-    const result = unflatten(input, '/');
+    const result = unflatten(input, "/");
     console.log(result);
     expect(result).toEqual(expected);
   });
 
-  it('should use custom delimiter', () => {
-    const input = { 'a': 1, 'b-c': 2, 'b-d': 3 };
+  it("should use custom delimiter", () => {
+    const input = { a: 1, "b-c": 2, "b-d": 3 };
     const expected = { a: 1, b: { c: 2, d: 3 } };
-    const result = unflatten(input, '-');
+    const result = unflatten(input, "-");
     expect(result).toEqual(expected);
   });
 
-
+  it("should return exactly the same object readed", () => {
+    const obj = require("./unflatten-obj.json");
+    const flattenValue = flatten(obj, { delimiter: "/" });
+    console.log(flattenValue);
+    const unflattenValue = unflatten(flattenValue, "/");
+    console.log(unflattenValue);
+    expect(JSON.stringify(flattenValue)).toEqual(
+      JSON.stringify(unflattenValue)
+    );
+  });
 });
